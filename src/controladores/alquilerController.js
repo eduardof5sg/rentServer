6f88f9c59f64baab3e7991fbfd809138ff31500c
eduardof5sg <149663359+eduardof5sg@users.noreply.github.com>
2025-04-19 11:28:10 +0200

    metodo eliminar solicitud alquiler correcto

--- src/controladores/alquilerController.js
+++ src/controladores/alquilerController.js
@@ -72,3 +72,39 @@ export const solicitudjuego = async(req,res) =>{
         res.status(500).json({ message: "Error al obtener las solicitudes." });
     }
 }
+
+export const eliminarSolicitud = async (req, res) => {
+    try {
+      const solicitudid = req.params.solicitudid;
+      const { propietario } = req.body;
+  
+      if (!solicitudid || !propietario) {
+        return res.status(400).json({ message: "Faltan datos necesarios" });
+      }
+  
+      // Buscar el alquiler
+      const alquiler = await alquilerModel.findById({_id:solicitudid});
+  
+      if (!alquiler) {
+        return res.status(404).json({ message: "Solicitud de alquiler no encontrada" });
+      }
+  
+      if (alquiler.propietario.toString() !== propietario) {
+        return res.status(403).json({ message: "No tienes permiso para eliminar esta solicitud" });
+      }
+  
+      // Cambiar la disponibilidad del juego a true
+      await juegoModel.findByIdAndUpdate(alquiler.juegoid, {
+        disponibilidad: true,
+      });
+  
+      // Eliminar la solicitud de alquiler
+      await alquilerModel.findByIdAndDelete(solicitudid);
+  
+      res.status(200).json({ message: "Solicitud eliminada y disponibilidad actualizada" });
+    } catch (error) {
+      console.error("Error al eliminar solicitud:", error);
+      res.status(500).json({ message: "Error al eliminar la solicitud" });
+    }
+  };
+  
--- src/rutas/alquilerRutas.js
+++ src/rutas/alquilerRutas.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { confirmarAlquiler, solicitudAlquiler, solicitudjuego } from "../controladores/alquilerController.js";
+import { confirmarAlquiler, eliminarSolicitud, solicitudAlquiler, solicitudjuego } from "../controladores/alquilerController.js";
 
 const alquilerRoutes = express.Router();
 
 alquilerRoutes.post('/:juegoid', solicitudAlquiler);
 alquilerRoutes.put('/confirmar/:solicitudid', confirmarAlquiler);
-alquilerRoutes.get('/solicitudes/:userid',solicitudjuego)
+alquilerRoutes.get('/solicitudes/:userid',solicitudjuego);
+alquilerRoutes.delete('/:solicitudid' , eliminarSolicitud)
 
 export default alquilerRoutes;
